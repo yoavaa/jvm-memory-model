2a9e181e6a6c27ebe916bebe824154425bc6e367
yoav@wix.com <yoav@wix.com>
2014-11-26 12:10:47 +0200

    refactor visibility app

--- src/main/scala/com/wix/Sync101.scala
+++ src/main/scala/com/wix/Sync101.scala
@@ -6,11 +6,11 @@ import java.util.concurrent.atomic.AtomicInteger
  * Created by yoava on 7/26/14.
  */
 object Sync101 extends App {
-  var count = new AtomicInteger(0)
+  var count = 0
   val threads: List[Thread] = List.fill(10)(new Thread(new Runnable() {
     def run() {
         for (i <- 1 to 1000)
-          count.incrementAndGet()
+          count = count + 1
     }
   }))
 
--- src/main/scala/com/wix/Visability.scala
+++ src/main/scala/com/wix/Visability.scala
@@ -1,32 +1,79 @@
 package com.wix
 
+import scala.io.StdIn
+
 /**
  * Created by yoava on 7/26/14.
  */
 object Visability extends App {
 
-  var running = true
+  println(
+    """Which test to run?
+      |1. regular var
+      |2. volatile
+    """.stripMargin)
+  val input = StdIn.readLine()
+  input match {
+    case "1" => regular()
+    case "2" => volatile()
+    case _ => "What???"
+  }
 
-  val t1 = new Thread(new Runnable {
-    def run() {
-      var count = 0
-      while (running)
-        count = count + 1
-      println(count)
-    }
-  })
 
-  val t2 = new Thread(new Runnable {
-    def run() {
-      Thread.sleep(100)
-      running = false
-      println("thread 2 set running to false")
-    }
-  })
+  def regular() {
 
-  t1.start()
-  t2.start()
+    var running = true
 
-  t1.join()
-  t2.join()
+    val t1 = new Thread(new Runnable {
+      def run() {
+        var count = 0
+        while (running)
+          count = count + 1
+        println(count)
+      }
+    })
+
+    val t2 = new Thread(new Runnable {
+      def run() {
+        Thread.sleep(100)
+        running = false
+        println("thread 2 set running to false")
+      }
+    })
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+  }
+
+  def volatile() {
+
+    @volatile var running = true
+
+    val t1 = new Thread(new Runnable {
+      def run() {
+        var count = 0
+        while (running)
+          count = count + 1
+        println(count)
+      }
+    })
+
+    val t2 = new Thread(new Runnable {
+      def run() {
+        Thread.sleep(100)
+        running = false
+        println("thread 2 set running to false")
+      }
+    })
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+  }
+
 }
