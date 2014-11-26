8abbd05a27858a210e416547cf0913aaea8a6c0e
yoav@wix.com <yoav@wix.com>
2014-11-26 15:44:32 +0200

    refactor sync app

--- src/main/scala/com/wix/Sync101.scala
+++ src/main/scala/com/wix/Sync101.scala
@@ -1,22 +1,112 @@
 package com.wix
 
 import java.util.concurrent.atomic.AtomicInteger
+import scala.io.StdIn
+import java.util.concurrent.locks.ReentrantLock
 
 /**
  * Created by yoava on 7/26/14.
  */
 object Sync101 extends App {
-  var count = 0
-  val threads: List[Thread] = List.fill(10)(new Thread(new Runnable() {
-    def run() {
+
+  println(
+    """Which test to run?
+      |1. counter
+      |2. volatile counter
+      |3. lock counter - synchronized
+      |4. lock counter - re-entrant lock
+      |5. atomic counter
+    """.stripMargin)
+  val input = StdIn.readLine()
+  input match {
+    case "1" => doCountInt()
+    case "2" => doCountVolatile()
+    case "3" => doCountSync()
+    case "4" => doCountReentrantLock()
+    case "5" => doCountAtomic()
+    case _ => "What???"
+  }
+
+
+  def doCountInt() {
+    var countInt = 0
+
+    val threads: List[Thread] = List.fill(10)(new Thread(new Runnable() {
+      def run() {
         for (i <- 1 to 1000)
-          count = count + 1
-    }
-  }))
+          countInt = countInt + 1
+      }
+    }))
 
-  threads.foreach(_.start)
+    threads.foreach(_.start)
+    threads.foreach(_.join)
+    println(countInt)
+  }
 
-  threads.foreach(_.join)
+  def doCountVolatile() {
+    @volatile var countVol = 0
 
-  println(count)
+    val threads: List[Thread] = List.fill(10)(new Thread(new Runnable() {
+      def run() {
+        for (i <- 1 to 1000)
+          countVol = countVol + 1
+      }
+    }))
+
+    threads.foreach(_.start)
+    threads.foreach(_.join)
+    println(countVol)
+  }
+
+  def doCountSync() {
+    var countSync = 0
+
+    val threads: List[Thread] = List.fill(10)(new Thread(new Runnable() {
+      def run() {
+        for (i <- 1 to 1000)
+          synchronized {
+            countSync = countSync + 1
+          }
+      }
+    }))
+
+    threads.foreach(_.start)
+    threads.foreach(_.join)
+    println(countSync)
+  }
+
+  def doCountReentrantLock() {
+    var countLock = 0
+    val lock = new ReentrantLock()
+
+    val threads: List[Thread] = List.fill(10)(new Thread(new Runnable() {
+      def run() {
+        for (i <- 1 to 1000) {
+          lock.lock()
+          countLock = countLock + 1
+          lock.unlock()
+        }
+      }
+    }))
+
+    threads.foreach(_.start)
+    threads.foreach(_.join)
+    println(countLock)
+  }
+
+  def doCountAtomic() {
+    val count = new AtomicInteger(0)
+
+    val threads: List[Thread] = List.fill(10)(new Thread(new Runnable() {
+      def run() {
+        for (i <- 1 to 1000) {
+          count.incrementAndGet()
+        }
+      }
+    }))
+
+    threads.foreach(_.start)
+    threads.foreach(_.join)
+    println(count.get)
+  }
 }
